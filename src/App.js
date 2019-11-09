43828253f602ca1966f180f2873f6fcc87a518ea
Baraka Larbi <laridev@gmail.com>
2019-11-09 01:03:06 +0100

    creating the first logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [valid, setValid] = useState(false);
+
+  const handleClick = (e) =>{
+
+    console.log('handle click')
+  }
+  const handleInput = (e) =>{
+    setEmail(e.target.value)
+  }
   return (
     <div className="App">
-    
+      <input name="email" type="email" value={email} onChange={handleInput}/>
+      <button role="button" onClick={handleClick}>Validate !</button>
+      <br/>
+      {email==="" ?  "" : <h3>this email : {email} is {valid ? "valid" : "not valid"}</h3>}
 
     </div>
   );
