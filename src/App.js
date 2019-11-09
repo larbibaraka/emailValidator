3bd2e57e186cc5f58f82eaf397e9357c19c97157
Baraka Larbi <laridev@gmail.com>
2019-11-09 01:10:28 +0100

    finish app logic with a simple bug

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ function App() {
   const [email, setEmail] = useState('');
   const [valid, setValid] = useState(false);
 
-  const handleClick = (e) =>{
-
-    console.log('handle click')
+  const handleClick = async(e) =>{
+    const url = `https://pozzad-email-validator.p.rapidapi.com/emailvalidator/validateEmail/${email}`;
+    const response  = await fetch(url, {
+    	"headers": {
+        "x-rapidapi-host": "pozzad-email-validator.p.rapidapi.com",
+        "x-rapidapi-key": "9c4837d252msh2a0cd163ca7c47bp1702dejsnf7e2cbcc8a04"
+      }
+    });
+    const {isValid} = await response.json();
+    setValid(isValid);
   }
   const handleInput = (e) =>{
+    setEmail("")
     setEmail(e.target.value)
   }
   return (
@@ -17,8 +25,7 @@ function App() {
       <input name="email" type="email" value={email} onChange={handleInput}/>
       <button role="button" onClick={handleClick}>Validate !</button>
       <br/>
-      {email==="" ?  "" : <h3>this email : {email} is {valid ? "valid" : "not valid"}</h3>}
-
+      {!valid ?  "" : <h3>Email : {email} is {valid ? "valid" : "not valid"}</h3>}
     </div>
   );
 }
