c2d5f2508e54a0a2a57307a5321d7d0945d34a22
Baraka Larbi <laridev@gmail.com>
2019-11-09 12:28:19 +0100

    Updates

---
+++ precache-manifest.0791b8fc37c58d0c54960a59cbd0cca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afba3bc7877bb5ec1df2ce137a1b6fc8",
+    "url": "/emailValidator/index.html"
+  },
+  {
+    "revision": "dd2ec0c45ace3648f6cd",
+    "url": "/emailValidator/static/css/main.17412ca3.chunk.css"
+  },
+  {
+    "revision": "27dd79b0b7fafa8feaeb",
+    "url": "/emailValidator/static/js/2.898f508d.chunk.js"
+  },
+  {
+    "revision": "dd2ec0c45ace3648f6cd",
+    "url": "/emailValidator/static/js/main.a7da21ce.chunk.js"
+  },
+  {
+    "revision": "c218560db1734a55b650",
+    "url": "/emailValidator/static/js/runtime-main.c527ae05.js"
+  }
+]);
